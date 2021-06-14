11ac87a12edc7dc198818c3ec2485cd4cbbf10e3
cotton-sspms <84834330+cotton-sspms@users.noreply.github.com>
2021-06-14 15:39:11 +0700

    Printing & Operator
    Printing Screen & Arithmetic Operator test

---
+++ Operator.java
@@ -0,0 +1,12 @@
+public class Operator {
+    public static void main(String[] args){
+        int x = 2, y = 3; 
+
+        System.out.println(x + " + " + y + " = " +(x+y));
+        System.out.println(x + " - " + y + " = " +(x-y));
+        System.out.println(x + " * " + y + " = " +(x*y));
+        System.out.println(x + " / " + y + " = " +(x/y)); //การหาร
+        System.out.println(x + " % " + y + " = " +(x%y)); //การหาเศษที่ได้จากการหาร
+
+    }
+}
---
+++ Screen.java
@@ -0,0 +1,10 @@
+class Screen{
+    public static void main(String[] args){
+        /*System.out.print("5");
+        System.out.print(" ");
+        System.out.print("10");*/
+
+        //short
+        System.out.print("5"+ " "+ "10");   // + การต่อ String
+    }
+}
